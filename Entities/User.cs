0413734c1821573ba8907761667c2f834f9517cf
Andre <andrefreitas09876@gmail.com>
2026-03-01 19:23:07 -0300

    feat: add class Users

---
+++ Entities/User.cs
@@ -0,0 +1,17 @@
+﻿namespace PersonalFinance.API.Entities
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public Guid UserId { get; set; }
+        public User(string name, int age, string email)
+        {
+            Name = name;
+            Age = age;
+            Email = email;
+            UserId = Guid.NewGuid();
+        }
+    }
+}
