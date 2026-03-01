2de8554c0bfbc6e9f0c58842f35207ba776fc8d1
Andre <andrefreitas09876@gmail.com>
2026-03-01 18:58:22 -0300

    feat: add class Transactions

---
+++ Entities/Transactions.cs
@@ -0,0 +1,27 @@
+﻿using PersonalFinance.API.Enums;
+using System.ComponentModel;
+
+namespace PersonalFinance.API.Entities
+{
+    public class Transactions
+    {
+        public Guid UserId { get; set; }
+        public Guid TransactionId { get; set; }
+        public decimal Value { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public Category Category { get; set; }
+        public string DescriptionTransaction { get; set; } = "";
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+
+        public Transactions(Guid userId, Guid transactionId, decimal value, TransactionType transactionType, Category category, string descriptionTransaction, DateTime date = default)
+        {
+            UserId = userId;
+            TransactionId = transactionId;
+            Value = value;
+            TransactionType = transactionType;
+            Category = category;
+            DescriptionTransaction = descriptionTransaction == default ? "" : descriptionTransaction;
+            Date = date == default ? DateTime.UtcNow : date;
+        }
+    }
+}
