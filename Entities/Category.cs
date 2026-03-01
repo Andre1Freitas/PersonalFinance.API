bd620022e9119cd165d10c3783cf5c53ee64f065
Andre <andrefreitas09876@gmail.com>
2026-03-01 19:12:04 -0300

    feat: add class Category

---
+++ Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿namespace PersonalFinance.API.Entities
+{
+    public class Category
+    {
+        public string Name { get; set; }
+        public Guid CategoryId { get; set; }
+        public Guid UserId { get; set; }
+
+        public Category(string name, Guid categoryId, Guid userId)
+        {
+            Name = name;
+            CategoryId = categoryId;
+            UserId = userId;
+        }
+    }
+}
--- Entities/Transactions.cs
+++ Entities/Transactions.cs
@@ -9,18 +9,18 @@ namespace PersonalFinance.API.Entities
         public Guid TransactionId { get; set; }
         public decimal Value { get; set; }
         public TransactionType TransactionType { get; set; }
-        public Category Category { get; set; }
-        public string DescriptionTransaction { get; set; } = "";
+        public Guid CategoryId { get; set; }
+        public string DescriptionTransaction { get; set; }
         public DateTime Date { get; set; } = DateTime.UtcNow;
 
-        public Transactions(Guid userId, Guid transactionId, decimal value, TransactionType transactionType, Category category, string descriptionTransaction, DateTime date = default)
+        public Transactions(Guid userId, Guid transactionId, decimal value, TransactionType transactionType, Guid categoryId, string descriptionTransaction = "", DateTime date = default)
         {
             UserId = userId;
             TransactionId = transactionId;
             Value = value;
             TransactionType = transactionType;
-            Category = category;
-            DescriptionTransaction = descriptionTransaction == default ? "" : descriptionTransaction;
+            CategoryId = categoryId;
+            DescriptionTransaction = descriptionTransaction;
             Date = date == default ? DateTime.UtcNow : date;
         }
     }
