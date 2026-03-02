6061b644015bbc7ae1bf9bf1fa5810e315e01657
Andre <andrefreitas09876@gmail.com>
2026-03-02 20:14:38 -0300

    feat: add AppDbContext in Data

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalFinance.API.Entities;
+
+namespace PersonalFinance.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Transactions> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
