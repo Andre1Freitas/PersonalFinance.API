c01141aaed84904c491cd711fcbf0c03e128043e
Andre <andrefreitas09876@gmail.com>
2026-03-04 19:30:18 -0300

    feat: add Result in Common

---
+++ Common/Result.cs
@@ -0,0 +1,17 @@
+﻿namespace PersonalFinance.API.Common
+{
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public string Error { get; }
+
+        private Result(bool isSuccess, string error)
+        {
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result Success() => new Result(true, "");
+        public static Result Failure(string error) => new Result(false, error);
+    }
+}
