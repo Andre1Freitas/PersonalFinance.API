e1c516525635ec3e89dc04b8d15d25929de9fa8b
Andre <andrefreitas09876@gmail.com>
2026-03-07 19:10:33 -0300

    refactor: add generic Result<T> class

--- Common/Result.cs
+++ Common/Result.cs
@@ -5,7 +5,7 @@
         public bool IsSuccess { get; }
         public string Error { get; }
 
-        private Result(bool isSuccess, string error)
+        public Result(bool isSuccess, string error)
         {
             IsSuccess = isSuccess;
             Error = error;
@@ -13,5 +13,17 @@
 
         public static Result Success() => new Result(true, "");
         public static Result Failure(string error) => new Result(false, error);
+    }
+    public class Result<T> : Result
+    {
+        public T? Value { get; }
+
+        private Result(bool isSuccess, string error, T? value) : base(isSuccess, error)
+        {
+            Value = value;
+        }
+
+        public static Result<T> Success(T value) => new Result<T>(true, "", value);
+        public static new Result<T> Failure(string error) => new Result<T>(false, error, default);
     }
 }
