d2c26be90eb73e04d2f1f7b2670f21b872252632
Andre <andrefreitas09876@gmail.com>
2026-03-08 18:04:53 -0300

    feat: add classes CreateCategoryDto and UpdateCategoryDto

---
+++ DTOs/CreateCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PersonalFinance.API.DTOs
+{
+    public class CreateCategoryDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public Guid UserId { get; set; }
+    }
+}
---
+++ DTOs/UpdateCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PersonalFinance.API.DTOs
+{
+    public class UpdateCategoryDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public Guid UserId { get; set; }
+    }
+}
