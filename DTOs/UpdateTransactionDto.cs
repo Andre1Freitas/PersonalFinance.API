f5760238e95b869bd7a6e2ef68cafb9e9b1a72e5
Andre <andrefreitas09876@gmail.com>
2026-03-08 18:04:13 -0300

    feat: add classes CreateTransactionDto and UpdateTransactionDto

---
+++ DTOs/UpdateTransactionDto.cs
@@ -0,0 +1,14 @@
+﻿using PersonalFinance.API.Enums;
+
+namespace PersonalFinance.API.DTOs
+{
+    public class UpdateTransactionDto
+    {
+        public Guid UserId { get; set; }
+        public decimal Value { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public Guid CategoryId { get; set; }
+        public string DescriptionTransaction { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+    }
+}
