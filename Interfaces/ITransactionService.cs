117d1853de48d633bef515bbbac3e0ba96873dc2
Andre <andrefreitas09876@gmail.com>
2026-03-09 18:54:11 -0300

    feat: add interface ITransaction.cs

---
+++ Interfaces/ITransactionService.cs
@@ -0,0 +1,15 @@
+﻿using PersonalFinance.API.Common;
+using PersonalFinance.API.Entities;
+
+namespace PersonalFinance.API.Interfaces
+{
+    public interface ITransactionService
+    {
+        public Result Add(Transactions transaction);
+        public Result Remove(Guid transactionId);
+        public Result Update(Guid transactionId, Transactions updatedTransaction);
+        public Result<Transactions?> GetById(Guid transactionId);
+        public Result<List<Transactions>> GetAllByUser(Guid userId);
+        public Result<List<Transactions>> GetPerPeriod(Guid userId, DateTime begin, DateTime end);
+    }
+}
