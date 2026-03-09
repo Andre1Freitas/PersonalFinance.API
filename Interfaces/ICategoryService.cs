a5ef66cb1398ea74d59aa7152b49b1256a67a3e1
Andre <andrefreitas09876@gmail.com>
2026-03-09 18:53:40 -0300

    feat: add interface ICategoryService

---
+++ Interfaces/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using PersonalFinance.API.Common;
+using PersonalFinance.API.Entities;
+
+namespace PersonalFinance.API.Interfaces
+{
+    public interface ICategoryService
+    {
+        public Result Add(Category category);
+        public Result Remove(Guid categoryId);
+        public Result Update(Guid categoryId, Category updatedCategory);
+        public Result<Category?> GetById(Guid categoryId);
+        public Result<List<Category>> GetAll();
+    }
+}
