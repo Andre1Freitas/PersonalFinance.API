837687ba9dd764575f86d745aa240d71fb7d9f37
Andre <andrefreitas09876@gmail.com>
2026-03-09 18:47:51 -0300

    feat: add interface IUserService

---
+++ Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using PersonalFinance.API.Entities;
+using PersonalFinance.API.Common;
+
+namespace PersonalFinance.API.Interfaces
+{
+    public interface IUserService
+    {
+        public Result Add(User user);
+        public Result Remove(Guid userId);
+        public Result Update(Guid userId, User updatedUser);
+        public Result<User> GetById(Guid userId);
+        public Result<List<User>> GetAll();
+
+    }
+}
